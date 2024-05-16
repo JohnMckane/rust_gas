e0f83ffc0aa5d7ff7ca9a4405944d6e72f182bba
john <jmckane07@qub.ac.uk>
2024-05-16 23:16:13 +0100

    Can now instantiate sample

--- basic_ga/src/main.rs
+++ basic_ga/src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
+use rand::Rng;
 fn main() {
     //Parameters
     let mut n_generations: i32 = 10;
     let mut  n_samples: i32 = 10;
     let args: Vec<String> = env::args().collect();
+    let mut rng = rand::thread_rng();
     //handle args
     for arg in args.iter() {
         let parts: Vec<&str> = arg.split(|c| c == '=').collect();
@@ -20,4 +22,9 @@ fn main() {
     }
     println!("Samples: {}", n_samples);
     println!("Generations: {}", n_generations);
+    //Create Gene pool
+    let mut pool: Vec<u32> = Vec::new();
+    for _i in 0..n_samples {
+        pool.push(rng.gen::<u32>());
+    }
 }

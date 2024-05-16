2e65f0a01f30f0e3cac4c5f65be2093e1159f108
john <jmckane07@qub.ac.uk>
2024-05-16 22:35:07 +0100

    Can now take commandline args

--- basic_ga/src/main.rs
+++ basic_ga/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    //Parameters
+    let mut n_generations: i32 = 10;
+    let mut  n_samples: i32 = 10;
+    let args: Vec<String> = env::args().collect();
+    //handle args
+    for arg in args.iter() {
+        let parts: Vec<&str> = arg.split(|c| c == '=').collect();
+        if parts.len() < 2 {
+            continue;
+        }
+        let setting = parts[0];
+        let value: i32 = parts[1].parse::<i32>().expect("Should"); 
+        match setting {
+            "--generations" => n_generations = value,
+            "--samples" => n_samples = value,
+            &_ => println!("Invalid Arg: {}", setting),
+        }
+    }
+    println!("Samples: {}", n_samples);
+    println!("Generations: {}", n_generations);
 }

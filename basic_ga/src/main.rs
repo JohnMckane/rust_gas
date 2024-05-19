3d086fd92feb64490c7c8142f03aea4a692946f2
john <jmckane07@qub.ac.uk>
2024-05-19 18:26:17 +0100

    Added score function

--- basic_ga/src/main.rs
+++ basic_ga/src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
 use rand::Rng;
+fn score(genes: &Vec<u32>) -> Vec<f64>{
+    let mut scores: Vec<f64> =  Vec::new();
+    scores
+}
 fn main() {
     //Parameters
     let mut n_generations: i32 = 10;
@@ -26,5 +30,9 @@ fn main() {
     let mut pool: Vec<u32> = Vec::new();
     for _i in 0..n_samples {
         pool.push(rng.gen::<u32>());
+    }
+    //Evolution Process.
+    for _i in 0..n_generations {
+        let scores: Vec<f64> = score(&pool);
     }
 }
